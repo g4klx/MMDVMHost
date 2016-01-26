eaae483f96495489f8d8dad8ecd045a98e57052f
Jonathan Naylor <naylorjs@yahoo.com>
2016-01-26 17:21:36 +0000

    Fix Linux compiler warning on format string.

--- HomebrewDMRIPSC.cpp
+++ HomebrewDMRIPSC.cpp
@@ -397,7 +397,7 @@ bool CHomebrewDMRIPSC::writeConfig()
 	::memcpy(buffer + 0U, "RPTC", 4U);
 	::memcpy(buffer + 4U, m_id, 4U);
 
-	::sprintf(buffer + 8U, "%-8.8s%09u%09u%02u%02u%-08f%-09f%03d%-20.20s%-20.20s%-124.124s%-40.40s%-40.40s", m_callsign.c_str(),
+	::sprintf(buffer + 8U, "%-8.8s%09u%09u%02u%02u%08f%09f%03d%-20.20s%-20.20s%-124.124s%-40.40s%-40.40s", m_callsign.c_str(),
 		m_rxFrequency, m_txFrequency, m_power, m_colorCode, m_latitude, m_longitude, m_height, m_location.c_str(),
 		m_description.c_str(), m_url.c_str(), m_software, m_version);
 
