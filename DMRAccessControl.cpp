39ee0f2739ddfdf585aa5c944d963b4de9d8c126
Simon <simon@adlem.net>
2016-10-05 12:04:00 +0100

    Tabs

--- DMRAccessControl.cpp
+++ DMRAccessControl.cpp
@@ -241,7 +241,7 @@ unsigned int DMRAccessControl::DstIdRewrite (unsigned int did, unsigned int sid,
 	      LogMessage("DMR Slot %u, Rewrite outbound private call to %u Group Call (Connect talkgroup by private call)",slot,did);
 	      return did;
   } else if (m_BMAutoRewrite && (did < 4000 || did > 5000) && did > 0 && did !=9 && did > 99999) {
-      m_dstRewriteID = did;
+	      m_dstRewriteID = did;
   } 
   return 0U;
 }
