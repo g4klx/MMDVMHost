398b9964a1abb2727ad2e768fed2acd7d37e10b6
Alex Koren <AlexKoren@actiview.local>
2016-11-04 11:44:38 -0700

    added license to nextion.py

--- Nextion/nextion.py
+++ Nextion/nextion.py
@@ -1,10 +1,28 @@
+'''
+ *   Copyright (C) 2016 Alex Koren
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+'''
+
 import serial
 import time
 import sys
 import os
 
 e = "\xff\xff\xff"
-deviceName = '/dev/ttyUSB0' #MAKE SURE THIS IS THE CORRECT DEVICE
+deviceName = '/dev/tty.usbserial-AK05BVRH' #MAKE SURE THIS IS THE CORRECT DEVICE
 CHECK_MODEL = 'NX3224T024'
 
 def getBaudrate(ser, fSize=None):
