e720e1e4c8811bedb4b87bf53ac445d09908e7f5
Andy CA6JAU <juribeparada@gmail.com>
2016-11-18 00:45:10 -0300

    Possible fix of loop after Internet down?

--- DMRNetwork.cpp
+++ DMRNetwork.cpp
@@ -559,6 +559,7 @@ bool CDMRNetwork::write(const unsigned char* data, unsigned int length)
 	bool ret = m_socket.write(data, length, m_address, m_port);
 	if (!ret) {
 		LogError("DMR, Socket has failed when writing data to the master, retrying connection");
+		m_socket.close();
 		open();
 		return false;
 	}
