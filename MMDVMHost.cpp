6844085db36cb912af436a8f02b840c197e5565b
Jonathan Naylor <naylorjs@yahoo.com>
2017-03-10 08:41:56 +0000

    Updated the copyright dates.

--- MMDVMHost.cpp
+++ MMDVMHost.cpp
@@ -74,7 +74,7 @@ static void sigHandler(int signum)
 const char* HEADER1 = "This software is for use on amateur radio networks only,";
 const char* HEADER2 = "it is to be used for educational purposes only. Its use on";
 const char* HEADER3 = "commercial networks is strictly prohibited.";
-const char* HEADER4 = "Copyright(C) 2015, 2016 by Jonathan Naylor, G4KLX and others";
+const char* HEADER4 = "Copyright(C) 2015-2017 by Jonathan Naylor, G4KLX and others";
 
 int main(int argc, char** argv)
 {
