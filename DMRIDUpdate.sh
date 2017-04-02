391b57f7abbe566552da43a4248b30a4a5859f35
Jonathan Naylor <naylorjs@yahoo.com>
2017-04-02 13:42:24 +0100

    Update the URL for DMR-MARC

--- DMRIDUpdate.sh
+++ DMRIDUpdate.sh
@@ -90,7 +90,7 @@ then
 fi
 
 # Generate new file
-curl 'http://www.dmr-marc.net/cgi-bin/trbo-database/datadump.cgi?table=users&format=csv&header=0' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > ${DMRIDFILE}
+curl 'http://dmr-marc.net/cgi-bin/trbo-database/datadump.cgi?table=users&format=csv&header=0' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > ${DMRIDFILE}
 
 # Restart MMDVMHost
 eval ${RESTARTCOMMAND}
