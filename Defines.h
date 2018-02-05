4323c02a0aca78d06681e04411c39b7deccb7b64
Jonathan Naylor <naylorjs@yahoo.com>
2018-02-05 19:46:27 +0000

    Add a proper bypass for the jitter buffer if wanted.

--- Defines.h
+++ Defines.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2015,2016,2017 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2015,2016,2017,2018 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -55,6 +55,12 @@ enum RPT_NET_STATE {
 	RS_NET_IDLE,
 	RS_NET_AUDIO,
 	RS_NET_DATA
+};
+
+enum B_STATUS {
+	BS_NO_DATA,
+	BS_DATA,
+	BS_MISSING
 };
 
 #endif
