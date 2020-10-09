1f72bf16d85bc05c634654fb7f2d7aebfe46b256
SASANO Takayoshi <uaa@uaa.org.uk>
2020-10-10 04:48:59 +0900

    add OSX support to NetworkInfo.cpp (EXPERIMENTAL)

--- NetworkInfo.cpp
+++ NetworkInfo.cpp
@@ -26,13 +26,13 @@
 #include <clocale>
 
 #include <sys/types.h>
-#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 #include <ifaddrs.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
+#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 #include <sys/sysctl.h>
 #include <net/if.h>
 #include <net/route.h>
@@ -66,7 +66,7 @@ void CNetworkInfo::getNetworkInterface(unsigned char* info)
 
 	::strcpy((char*)info, "(address unknown)");
 
-#if defined(__linux__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 	char* dflt = NULL;
 
 #if defined(__linux__)
@@ -91,7 +91,7 @@ void CNetworkInfo::getNetworkInterface(unsigned char* info)
 
 	::fclose(fp);
 
-#elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
+#elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 	const int mib[] = {
 		CTL_NET,
 		PF_ROUTE,
@@ -126,7 +126,7 @@ void CNetworkInfo::getNetworkInterface(unsigned char* info)
 			continue;
 #if defined(__OpenBSD__)
 		struct sockaddr_in *sa = (struct sockaddr_in *)(p + rtm->rtm_hdrlen);
-#elif defined(__NetBSD__) || defined(__FreeBSD__)
+#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 		struct sockaddr_in *sa = (struct sockaddr_in *)(rtm + 1);
 #endif
 		if (sa->sin_addr.s_addr == INADDR_ANY) {
