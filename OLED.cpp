e444ec939295c241279dff8e3117a2f3489adb70
Paul Kronenwetter <kronenpj@gmail.com>
2021-01-16 13:56:20 +0000

    Update OLED.cpp
    Removed commented lines.

--- OLED.cpp
+++ OLED.cpp
@@ -660,14 +660,6 @@ void COLED::clearCWInt()
 {
     m_display.clearDisplay();
 
-    //m_display.setCursor(0,30);
-    //m_display.setTextSize(3);
-    //m_display.print("Idle");
-
-    //m_display.setTextSize(1);
-    //m_display.display();
-    //m_display.startscrollleft(0x02,0x0f);
-
     OLED_statusbar();
     m_display.display();
     if (m_displayScroll)
