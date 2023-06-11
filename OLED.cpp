216bfaa7bb89187a7480570ed8255227e7b94a76
Chipster <go@w.ay>
2023-06-11 14:14:39 -0500

    Fix version # per G4KLX, fix OLED IP spacing in idle mode, improve auto-ap stats in OLED

--- OLED.cpp
+++ OLED.cpp
@@ -285,7 +285,7 @@ void COLED::setIdleInt()
 	}
 	// Read ssid value from /etc/hostapd.conf if it exists...
 	std::string ssid;
-	std::ifstream configFile("/etc/hostapd.conf");
+	std::ifstream configFile("/etc/hostapd/hostapd.conf");
 	if (configFile.is_open()) {
 	    std::string line;
 	    while (std::getline(configFile, line)) {
@@ -306,10 +306,10 @@ void COLED::setIdleInt()
 	m_display.setCursor(0, OLED_LINE3);
 	m_display.setTextSize(1);
 	m_display.printf("Auto-AP Running...");
-	m_display.setCursor(0, OLED_LINE4);
+	m_display.setCursor(0, OLED_LINE5);
 	m_display.setTextSize(1);
 	m_display.printf("SSID: %s", ssid.c_str());
-	m_display.setCursor(0, OLED_LINE5);
+	m_display.setCursor(0, OLED_LINE6);
 	m_display.setTextSize(1);
 	m_display.printf("IP: %s", m_ipaddress.c_str());
     } else { // Connected to network - no Auto-AP mode; normal display layout...
@@ -317,7 +317,7 @@ void COLED::setIdleInt()
 	m_display.setTextSize(1);
 	m_display.print("        -IDLE-");
 	m_display.setCursor(0, OLED_LINE5);
-	m_display.printf("  %s", m_ipaddress.c_str());
+	m_display.printf("%s", m_ipaddress.c_str());
     }
 
     if (m_displayScroll && m_displayLogoScreensaver)
